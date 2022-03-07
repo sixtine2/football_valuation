d812fc4b15a933d89418a1f6d3d9580aafc84978
Sixtine Lamy <lamysixtine@gmail.com>
2022-03-07 16:20:45 +0100

    draft streamlit

--- streamlit_jacques/streamlit/app.py
+++ streamlit_jacques/streamlit/app.py
@@ -1,39 +1,78 @@
 import streamlit as st
-
 import datetime
-
 import requests
+import pandas as pd
+import numpy as np
+import plotly.express as px
+import plotly.graph_objects as go
 
-'''
-# TaxiFareModel front
 
-This front queries the Le Wagon [taxi fare model API](https://taxifare.lewagon.ai/predict?pickup_datetime=2012-10-06%2012:10:20&pickup_longitude=40.7614327&pickup_latitude=-73.9798156&dropoff_longitude=40.6513111&dropoff_latitude=-73.8803331&passenger_count=2)
-'''
+# url to request
+url = "https://raw.githubusercontent.com/sixtine2/football_valuation/master/streamlit_jacques/Data/test.csv"
+# pretty print JSON data
+df = pd.read_csv(url)
+df.columns = df.iloc[0]
+dict_player = df.loc[df['Player Name'] == 'Zlatan Ibrahimovic'].to_dict(orient='records')[0]
 
-pickup_date = st.date_input('pickup datetime', value=datetime.datetime(2012, 10, 6, 12, 10, 20))
-pickup_time = st.time_input('pickup datetime', value=datetime.datetime(2012, 10, 6, 12, 10, 20))
-pickup_datetime = f'{pickup_date} {pickup_time}'
-pickup_longitude = st.number_input('pickup longitude', value=40.7614327)
-pickup_latitude = st.number_input('pickup latitude', value=-73.9798156)
-dropoff_longitude = st.number_input('dropoff longitude', value=40.6413111)
-dropoff_latitude = st.number_input('dropoff latitude', value=-73.7803331)
-passenger_count = st.number_input('passenger_count', min_value=1, max_value=8, step=1, value=1)
+###
 
-# enter here the address of your flask api
-url = 'https://taxifare.lewagon.ai/predict'
+player_name = 'Lionel Messi'
+player_age = dict_player['age']
+position = 'Attack'
+games_played = 100
+goals = 10
+assists = 10
+rating = 7.5
+contract_expires = 2023
+spg = 3
+player_face = 'https://cdn.sofifa.net/players/158/023/22_120.png'
+club_logo = 'https://cdn.sofifa.net/teams/73/60.png'
+nation_logo = 'https://cdn.sofifa.net/teams/1369/60.png'
 
-params = dict(
-    pickup_datetime=pickup_datetime,
-    pickup_longitude=pickup_longitude,
-    pickup_latitude=pickup_latitude,
-    dropoff_longitude=dropoff_longitude,
-    dropoff_latitude=dropoff_latitude,
-    passenger_count=passenger_count)
+#st.set_page_config(layout="wide")
 
-response = requests.get(url, params=params)
+st.title(player_name)
+#st.image(player_face)
+#st.markdown("<img src='https://cdn.sofifa.net/players/158/023/22_120.png'")
 
-prediction = response.json()
+st.subheader('Key info:')
+col1, col2, col3 = st.columns(3)
+col1.metric("Position: ", str(position))
+col2.metric("Age: ", str(player_age))
+col3.metric("Contract expires in: ", str(contract_expires))
 
-pred = prediction['fare']
+#expander = st.expander('You can click here to see the raw data first 👉')
+#expander.dataframe(data=df.loc[df['Player Name'] == 'Zlatan Ibrahimovic'].to_dict(orient='records')[0])
 
-pred
+st.subheader('Recent performance snapshot:')
+col1, col2, col3, col4 = st.columns(4)
+with col1:
+    st.markdown("🏟️ " + str(games_played) + " Matches")
+with col2:
+    st.markdown("🥅 " + str(goals) + " Total Goals")
+with col3:
+    st.markdown("👟⚽ " + str(spg) + " Shots per Game")
+with col4:
+    st.markdown("🔁" + str(assists) + " Assists per Game")
+
+st.subheader('Market value bridge:')
+fig = go.Figure(go.Waterfall(
+    name = "20", orientation = "v",
+    measure = ["relative", "relative", "total", "relative", "total", "relative", "total"],
+    x = ["Sales cut", "Subs revenue", "Total revenue", "Reputational costs", "Profit before IT", "IT costs", "Profit after IT"],
+    textposition = "outside",
+    text = ["1.4M", "1.5M","2.8M", "-1.4M", "1.4M", "-0.5M", "0.9M"],
+    y = [1.4, 1.5, 2.8, -1.4, 1.4, -0.5, -0.5 -0.9],
+    connector = {"line":{"color":"rgb(210, 210, 210)"}}
+))
+
+fig.update_layout(
+        font_family="Helvetica",
+        #title = "Profit calculation",
+        showlegend = False,
+    plot_bgcolor='rgb(255,255,255)'
+)
+fig.update_layout(yaxis={'visible': False, 'showticklabels': False})
+fig.update_layout(xaxis={'visible': True, 'showticklabels': True}, xaxis_title=None)
+
+st.plotly_chart(fig)
