6b2ad230c7d15a2d0d480ffd4492bea358b5f853
Alexandre Sena <alexandre.sena@live.fr>
2022-03-10 18:38:44 +0100

    Some changes

--- setup.sh
+++ setup.sh
@@ -1,13 +1,13 @@
 mkdir -p ~/.streamlit/
 
-echo “\
+echo "\
 [general]\n\
-email = \“lamysixtine@gmail.com\“\n\
-” > ~/.streamlit/credentials.toml
+email = \"lamysixtine@gmail.com\"\n\
+"> ~/.streamlit/credentials.toml
 
-echo “\
+echo "\
 [server]\n\
 headless = true\n\
 enableCORS=false\n\
 port = $PORT\n\
-” > ~/.streamlit/config.toml
+" > ~/.streamlit/config.toml
